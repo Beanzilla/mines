872c353d4abf7e27868f4acecb6c6e611933f2b2
Apollo <beanzilla@beanzilla.net>
2022-01-07 16:39:12 -0500

    feat(api.remove_node): api's node handling is almost complete
    Found that we can add_node's but till this commit there was no way to remove_node's from a mine.

--- api.lua
+++ api.lua
@@ -1,6 +1,7 @@
 
 local api = mines
 
+-- Creates a mine given it's name, it's positions, it's list of nodes and their percents, and it's rate at which to regenerate in
 api.create_mine = function (mine_name, posA, posB, node_list, regen)
     if _mines.tools.addPercent(node_list) > 100 then
         _mines.tools.log("Percent exceeds 100, got "..tonumber(_mines.tools.addPercent(node_list)).."%")
@@ -14,11 +15,15 @@ api.create_mine = function (mine_name, posA, posB, node_list, regen)
         regen= regen, -- How many "ticks" till this mine regenerates
         timer=0, -- Current time (Set to regen when 0 or below 0, then spawn in terrain)
         tp= "", -- If defined, where to place players who were in the mine while it was about to regenerate
+        -- Should I also add owner? (maybe that as a priv?)
+        -- Just like spawning to this mine via /mines <mine_name> (omit mine_name to get list of mines) 
+        -- Using priv for spawning to particular mines? (maybe)
     }
     _mines.store:set_string("mines", minetest.serialize(m))
     return {success=true, errmsg="", val=nil}
 end
 
+-- Removes a mine and ALL it's data, just given the mine's name
 api.remove_mine = function (mine_name)
     local m = minetest.deserialize(_mines.store:get_string("mines")) or {}
     if _mines.tools.tableContainsValue(m, mine_name) then
@@ -37,6 +42,7 @@ api.remove_mine = function (mine_name)
     return {success=false, errmsg="Failed removing mine called '"..mine_name.."', wasn't found.", val=nil}
 end
 
+-- Returns ALL of the mine's data, just given the mine's name
 api.info_mine = function (mine_name)
     local m = minetest.deserialize(_mines.store:get_string("mines")) or {}
     if _mines.tools.tableContainsValue(m, mine_name) then
@@ -46,6 +52,8 @@ api.info_mine = function (mine_name)
     return {success=false, errmsg="Failed getting info on mine called '"..mine_name.."', wasn't found.", val=nil}
 end
 
+-- Sets the teleport point for spawning to a public mine and used to teleport players out of a mine that's regenerating
+-- Just give it the mine's name and the position
 api.set_mine_tp = function (mine_name, pos)
     local m = minetest.deserialize(_mines.store:get_string("mines")) or {}
     if _mines.tools.tableContainsValue(m, mine_name) then
@@ -54,23 +62,50 @@ api.set_mine_tp = function (mine_name, pos)
         return {success=true, errmsg="", val=nil}
     end
     _mines.tools.log("Failed setting mine teleport on mine called '"..mine_name.."', wasn't found.")
-    return {success=false, errmsg="Failed setting mine teleport on mine called '"..mine_name.."', wasn't found."}
+    return {success=false, errmsg="Failed setting mine teleport on mine called '"..mine_name.."', wasn't found.", val=nil}
 end
 
+-- Adds a node to a mine, given mine's name, node's itemstring and node's percent chance to be spawned in
 api.add_node = function (mine_name, node, perc)
     local m = minetest.deserialize(_mines.store:get_string("mines")) or {}
     if _mines.tools.tableContainsValue(m, mine_name) then
-        local tmp = m[mine_name].nodes or {}
-        table.insert(tmp, tostring(node).."="..tostring(_mines.tools.makePercent(perc)))
-        local tmp_perc = _mines.tools.addPercent(tmp)
+        local tmp_nodes = m[mine_name].nodes or {}
+        table.insert(tmp_nodes, tostring(node).."="..tostring(_mines.tools.makePercent(perc)))
+        local tmp_perc = _mines.tools.addPercent(tmp_nodes)
         if tmp_perc > 100 then
             _mines.tools.log("Percent exceeds 100, got "..tostring(tmp_perc).."%")
             return {success=false, errmsg="Percent exceeds 100, got "..tostring(tmp_perc).."%", val=nil}
         end
-        m[mine_name].nodes = tmp
+        m[mine_name].nodes = tmp_nodes
         _mines.store:set_string("mines", minetest.serialize(m))
         return {success=true, errmsg="", val=nil}
     end
     _mines.tools.log("Failed adding a node into mine called '"..mine_name.."', wasn't found.")
-    return {success=false, errmsg="Failed adding a node into mine called '"..mine_name.."', wasn't found."}
+    return {success=false, errmsg="Failed adding a node into mine called '"..mine_name.."', wasn't found.", val=nil}
+end
+
+-- Removes a node from a mine, given mine's name and node's itemstring
+api.remove_node = function (mine_name, node)
+    local m = minetest.deserialize(_mines.store:get_string("mines")) or {}
+    if _mines.tools.tableContainsValue(m, mine_name) then
+        local tmp_nodes = m[mine_name].nodes or {}
+        local idx = -1
+        for idex, value in ipairs(tmp_nodes) do
+            tmp_val = _mines.tools.split(value, "=")
+            if _mines.tools.contains(tmp_val, node) then
+                idx = idex
+            end
+        end
+        if idx ~= -1 then
+            table.remove(tmp_nodes, idex)
+            m[mine_name].nodes = tmp_nodes
+            _mines.store:set_string("mines", minetest.serialize(m))
+            return {success=true, errmsg="", val=nil}
+        else
+            _mines.tools.log("Failed removing node '"..node.."' from '"..mine_name.."', as node wasn't found.")
+            return {success=false, errmsg="Failed removing node '"..node.."' from '"..mine_name.."', as node wasn't found.", val=nil}
+        end
+    end
+    _mines.tools.log("Failed adding a node into mine called '"..mine_name.."', wasn't found.")
+    return {success=false, errmsg="Failed adding a node into mine called '"..mine_name.."', wasn't found.", val=nil}
 end
